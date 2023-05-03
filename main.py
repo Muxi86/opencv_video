fd75ec5dd363dcf3e875b6a24710ae906fb5139e
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-05-03 21:32:46 +0500

    waitkey

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ img = np.zeros((512,512,3), np.uint8)
 cv2.circle(img,(256,256), 100, (0,0,255), 2)
 cv2.imshow('Image', img)
 
-cv2.waitKey(0)
+while True:
+    k = cv2.waitKey(1)
+    print(k)
+    if k == 27:
+        break
 
 cv2.destroyAllWindows()
